50115431fc67e7f49fa67f94e152901f7f3fa4d7
guillaume dumoulin <97092035+Guillaume11Dev@users.noreply.github.com>
2023-09-12 11:44:26 +0200

    test

---
+++ main.ts
@@ -0,0 +1,58 @@
+class Animal {
+    constructor(
+        public id: number,
+        public nom: string,
+        public age: number,
+        public debutPret: Date,
+        public finPret: Date
+    ) {}
+
+    getId(): number {
+        return this.id;
+    }
+
+    getNom(): string {
+        return this.nom;
+    }
+    getAge():number {
+        return this.age;
+    }
+}
+
+class Employe {
+    constructor(
+        public id: number,
+        public nom: string,
+        public animal: Animal,
+    ) {}
+
+    travaillerAvec(animal: Animal): void {
+        console.log(`${this.nom} travaille avec ${animal.nom} le ${animal.constructor.name}.`);
+    }
+
+    assignerAnimal(animal: Animal): void {
+        console.log(`${this.nom} a reçu l'animal ${animal.nom}.`);
+    }
+
+
+}
+
+class Chien extends Animal {
+    faireDuBruit(): void {
+        console.log(`${this.nom} aboie.`);
+    }
+}
+
+class Chat extends Animal {
+    miauler(): void {
+        console.log(`${this.nom} miaule.`);
+    }
+}
+
+
+const chien1 = new Chien(1, "Buddy", 5, new Date("2023-01-01"), new Date("2023-01-03"));
+const chat1 = new Chat(2, "Whiskers", 3, new Date("2023-01-01"), new Date("2023-01-03"));
+const employe1 = new Employe(1, "John", chat1);
+const employe2 = new Employe(2, "Alice", chien1);
+
+
